7cd64124dbfc700a3cc567b3557d0e2a5e2b2367
gaisuke <danimunif@cs.unej.ac.id>
2026-08-01 07:48:53 +0000

    ci: add Cloud Run Dockerfile & deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   allowedDevOrigins: [
     "localhost:3000",
     "3000-cs-5e8b03fe-e556-44e3-8bab-0dba4f474baf.cs-asia-southeast1-ajrg.cloudshell.dev",
